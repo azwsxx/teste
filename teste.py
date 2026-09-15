3dfc18354cb083e15f7374bbfcb68b3aaf61fd8c
UFV <leocarpe@gmail.com>
2026-09-15 15:04:04 -0300

    mensagem

--- teste.py
+++ teste.py
@@ -1,3 +1,7 @@
-print("Ola! Qual seu nome?")
-name = input("Ola! Meu nome é: ")
-print("Ok!, Só pra ter certeza então! Seu nome é:",name)
+num= int(input("Agora Digite um número para ter sua piramide: ")) multiplier = 1
+while (num > 0):
+    num2 =((num+multiplier) - num)
+    num3 = (2*num2)-1
+    print (" "*num,"*"*num3, " "*num)
+    num = num - 1
+    multiplier= multiplier +1
